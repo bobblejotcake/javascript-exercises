b7ef3ae54c92188964a6d03c1804343e2fdac9d0
bobblejotcake <seanlimhe@gmail.com>
2026-01-29 15:45:24 +1000

    Completed Excercise 06-Repeat-Strings

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, repeats) {
+    if (repeats < 0){
+        return "ERROR";
+    }
 
+    let finalString = [];
+    for (let i =0; i < repeats; i++){
+        finalString.push(string);
+    }
+
+    return finalString.join('');
 };
 
 // Do not edit below this line
