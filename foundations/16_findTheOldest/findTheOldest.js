c9e806ebc8ce64be960fdbad5bb82dcd3212fff2
bobblejotcake <seanlimhe@gmail.com>
2026-02-09 15:17:26 +1000

    Completed Exe16 findtheOldest

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+function calculateAge(birthYear){
+  const currentYear = new Date().getFullYear();
+  return currentYear - birthYear;
+};
 
+const findTheOldest = function(array) {
+  const currentYear = new Date().getFullYear();
+  let ageArray = array.map((person) => {
+    //check if they have a year of Death
+    let age = person.yearOfDeath ? person.yearOfDeath - person.yearOfBirth : calculateAge(person.yearOfBirth);
+    return {name: person.name, age: age};
+  })
+  console.log(ageArray);
+  
+  ageArray.sort((a,b) => b.age - a.age);
+  
+  return ageArray[0];
+  
 };
 
 // Do not edit below this line
