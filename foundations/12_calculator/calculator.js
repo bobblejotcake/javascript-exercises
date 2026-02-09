df5e220ec280aa15edf378915032b9939f063834
bobblejotcake <seanlimhe@gmail.com>
2026-02-09 14:02:47 +1000

    Finished EXE12 Calculator

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,49 @@
-const add = function() {
+const add = function(num1, num2) {
+	return num1 + num2;
+};
+
+const subtract = function(num1, num2) {
+	return num1 - num2;
+};
+
+const sum = function(array) {
+  if(array.length == 0){
+    return 0;
+  }
+  else{
+    return array.reduce((total,number) =>  total += number)
+  }
 	
 };
 
-const subtract = function() {
-	
+const multiply = function(array) {
+  return array.reduce((total,number) => total *= number);
 };
 
-const sum = function() {
-	
+const power = function(base, exo) {
+	  if (exo == 0){
+    return 1;
+  }
+  else{
+    let exoArray = [];
+    for (let i = 0; i < exo; i++){
+      exoArray.push(base);
+    }
+    return multiply(exoArray);
+  }
 };
 
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(factNum) {
+	if(factNum == 0){
+    return 1
+  }
+  else{
+    let factArray = [];
+    for (let i=0; i < factNum; i++){
+      factArray.push(factNum-i);
+    }
+    return multiply(factArray);
+  }
 };
 
 // Do not edit below this line
