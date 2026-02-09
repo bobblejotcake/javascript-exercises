5c3f7d80e7e6b89b62be7f3fa14ea310a7add961
bobblejotcake <seanlimhe@gmail.com>
2026-02-09 14:50:36 +1000

    Completed EXE14 fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,34 @@
-const fibonacci = function() {
+function addLastTwo(array){
+  let num1 = array[array.length - 2];
+  let num2 = array[array.length - 1];
+  return num1 + num2;
+}
 
+const fibonacci = function(index) {
+ let trueIndex = parseInt(index);
+  if (trueIndex < 0){
+    return "OOPS"
+  }
+  let repeats = trueIndex - 2;
+  if (repeats < 0){
+    switch(trueIndex){
+      case 0:
+        return 0;
+        break
+      case 1:
+        return 1;
+        break
+      case 2:
+        return 2;
+        break
+    }
+  }
+  
+  let fiboArray = [1,1];
+  for (let i = 0; i < repeats; i++){
+    fiboArray.push(addLastTwo(fiboArray));
+  }
+  return fiboArray[trueIndex-1];
 };
 
 // Do not edit below this line
